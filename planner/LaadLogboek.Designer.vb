c1d1610b0ca3d86d839dec738b968ca665c05a86
Igor Katenov <igor.v.katenov@gmail.com>
2019-04-10 14:07:45 +0300

    Implemented LaadLogboek form.

---
+++ planner/LaadLogboek.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LaadLogboek
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LaadLogboek))
+        Me.iGrid1 = New TenTec.Windows.iGridLib.iGrid()
+        Me.IGrid1DefaultCellStyle = New TenTec.Windows.iGridLib.iGCellStyle(True)
+        Me.IGrid1DefaultColHdrStyle = New TenTec.Windows.iGridLib.iGColHdrStyle(True)
+        Me.IGrid1RowTextColCellStyle = New TenTec.Windows.iGridLib.iGCellStyle(True)
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.checkBoxTreeLines = New System.Windows.Forms.CheckBox()
+        CType(Me.iGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'iGrid1
+        '
+        Me.iGrid1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.iGrid1.DefaultCol.CellStyle = Me.IGrid1DefaultCellStyle
+        Me.iGrid1.DefaultCol.ColHdrStyle = Me.IGrid1DefaultColHdrStyle
+        Me.iGrid1.Header.Height = 20
+        Me.iGrid1.Location = New System.Drawing.Point(12, 12)
+        Me.iGrid1.Name = "iGrid1"
+        Me.iGrid1.Size = New System.Drawing.Size(1110, 307)
+        Me.iGrid1.TabIndex = 0
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "lock-icon.png")
+        Me.ImageList1.Images.SetKeyName(1, "add-1-icon.png")
+        '
+        'checkBoxTreeLines
+        '
+        Me.checkBoxTreeLines.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.checkBoxTreeLines.AutoSize = True
+        Me.checkBoxTreeLines.Location = New System.Drawing.Point(12, 332)
+        Me.checkBoxTreeLines.Name = "checkBoxTreeLines"
+        Me.checkBoxTreeLines.Size = New System.Drawing.Size(106, 17)
+        Me.checkBoxTreeLines.TabIndex = 1
+        Me.checkBoxTreeLines.Text = "Show Tree Lines"
+        Me.checkBoxTreeLines.UseVisualStyleBackColor = True
+        '
+        'LaadLogboek
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1134, 361)
+        Me.Controls.Add(Me.checkBoxTreeLines)
+        Me.Controls.Add(Me.iGrid1)
+        Me.Name = "LaadLogboek"
+        Me.Text = "Laad Logboek"
+        CType(Me.iGrid1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents iGrid1 As TenTec.Windows.iGridLib.iGrid
+    Friend WithEvents IGrid1DefaultCellStyle As TenTec.Windows.iGridLib.iGCellStyle
+    Friend WithEvents IGrid1DefaultColHdrStyle As TenTec.Windows.iGridLib.iGColHdrStyle
+    Friend WithEvents IGrid1RowTextColCellStyle As TenTec.Windows.iGridLib.iGCellStyle
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents checkBoxTreeLines As CheckBox
+End Class
---
+++ planner/LaadLogboek.vb
@@ -0,0 +1,85 @@
+﻿Imports TenTec.Windows.iGridLib
+
+Public Class LaadLogboek
+    Private Const WERKNEMER_LOGDE_WEBSITE As String = "Werknemer logde in op de website"
+    Private Const WERKNEMER_AANVAARDDE_WERKUREN As String = "Werknemer aanvaardde geplande werkuren"
+
+    Private Sub IconTreeGridSample_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With iGrid1
+            .BeginUpdate()
+
+            .ImageList = ImageList1
+            .ReadOnly = True
+            .RowMode = True
+            .DefaultRow.Height = 20
+
+            .Cols.Count = 7
+
+            .Cols(0).Text = "Datum logboek"
+            .Cols(1).Text = "Type log"
+            .Cols(2).Text = "Medewerker"
+            .Cols(3).Text = "Aanvang prestatie"
+            .Cols(4).Text = "Einde prestatie"
+            .Cols(5).Text = "Afdeling"
+            .Cols(6).Text = "Onderwerp"
+
+            Const DATE_FORMAT As String = "{0:MM/dd/yyyy HH:mm}"
+            .Cols(0).CellStyle.FormatString = DATE_FORMAT
+            .Cols(3).CellStyle.FormatString = DATE_FORMAT
+            .Cols(4).CellStyle.FormatString = DATE_FORMAT
+
+            .TreeCol = .Cols(0)
+
+            AddRecord(0, New Date(2019, 3, 31, 20, 41, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Liselotte Vergote")
+            AddRecord(0, New Date(2019, 3, 31, 19, 18, 0), 1, WERKNEMER_AANVAARDDE_WERKUREN, "Veronique Cineger", New Date(2019, 4, 12, 12, 0, 0), New Date(2019, 4, 12, 15, 30, 0), "Uitpak+magazijn")
+            AddRecord(1, New Date(2019, 3, 31, 19, 18, 0), 1, WERKNEMER_AANVAARDDE_WERKUREN, "Veronique Cineger", New Date(2019, 4, 12, 7, 0, 0), New Date(2019, 4, 12, 11, 0, 0), "Uitpak+magazijn")
+            AddRecord(1, New Date(2019, 3, 31, 19, 18, 0), 1, WERKNEMER_AANVAARDDE_WERKUREN, "Veronique Cineger", New Date(2019, 4, 10, 11, 30, 0), New Date(2019, 4, 10, 16, 30, 0), "Uitpak+magazijn")
+            AddRecord(1, New Date(2019, 3, 31, 19, 18, 0), 1, WERKNEMER_AANVAARDDE_WERKUREN, "Veronique Cineger", New Date(2019, 4, 10, 7, 0, 0), New Date(2019, 4, 10, 11, 0, 0), "Uitpak+magazijn")
+            AddRecord(0, New Date(2019, 3, 31, 19, 18, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Veronique Cineger")
+            AddRecord(0, New Date(2019, 3, 31, 19, 15, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Veronique Cineger")
+            AddRecord(0, New Date(2019, 3, 31, 18, 50, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Gianni Duwayn")
+            AddRecord(0, New Date(2019, 3, 31, 15, 9, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Carmen Rotsaert")
+            AddRecord(0, New Date(2019, 3, 31, 14, 49, 0), 0, WERKNEMER_LOGDE_WEBSITE, "Ann Gysel")
+
+            .Cols.AutoWidth()
+
+            .EndUpdate()
+        End With
+    End Sub
+
+    Private Sub AddRecord(ByVal pLevel As Integer, ByVal pDatumLogboek As Date, ByVal pImageIndex As Integer, ByVal pTypeLog As String,
+                          ByVal pMedewerker As String, Optional ByVal pAanvangPrestatie? As Date = Nothing, Optional ByVal pEindePrestatie? As Date = Nothing,
+                          Optional ByVal pAfdeling As String = Nothing, Optional pOnderwerp As String = Nothing)
+        With iGrid1
+            .BeginUpdate()
+
+            Dim row As iGRow = .Rows.Add()
+
+            row.Level = pLevel
+            If pLevel = 0 Then
+                row.TreeButton = iGTreeButtonState.Visible
+            Else
+                row.TreeButton = iGTreeButtonState.Hidden
+            End If
+
+            row.Cells(0).ImageIndex = pImageIndex
+            row.Cells(0).Value = pDatumLogboek
+            row.Cells(1).Value = pTypeLog
+            row.Cells(2).Value = pMedewerker
+            If pAanvangPrestatie.HasValue Then
+                row.Cells(3).Value = pAanvangPrestatie.Value
+            End If
+            If pEindePrestatie.HasValue Then
+                row.Cells(4).Value = pEindePrestatie.Value
+            End If
+            row.Cells(5).Value = pAfdeling
+            row.Cells(6).Value = pOnderwerp
+
+            .EndUpdate()
+        End With
+    End Sub
+
+    Private Sub checkBoxTreeLines_CheckedChanged(sender As Object, e As EventArgs) Handles checkBoxTreeLines.CheckedChanged
+        iGrid1.TreeLines.Visible = checkBoxTreeLines.Checked
+    End Sub
+End Class
